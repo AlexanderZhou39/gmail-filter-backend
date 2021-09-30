d7715559d1096596047e5ac9f7ea8d21f46efbba
Ray Cai <70855644+RayHCai@users.noreply.github.com>
2021-09-30 18:00:08 -0400

    Delete CollegeDespamifierAPI directory

--- CollegeDespamifierAPI/CollegeApp/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import College
-
-class CollegeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = College
-        fields = '__all__'
