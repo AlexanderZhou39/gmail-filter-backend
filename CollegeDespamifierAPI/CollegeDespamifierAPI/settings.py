b9410e1ea21365827583c33f0067d31534bd81f0
Jambles <jkintana@protonmail.com>
2021-10-18 17:22:48 -0700

    Initial
    Adds .env file. Changed settings.py to incorporate environment variables. Using django-environ.

--- CollegeDespamifierAPI/CollegeDespamifierAPI/settings.py
+++ CollegeDespamifierAPI/CollegeDespamifierAPI/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ # using django-environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r%!_3x6++6+np@jxk(v487_=0%a7l3vq7g&amo!glj-@lveg5'
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-2r%!_3x6++6+np@jxk(v487_=0%a7l3vq7g&amo!glj-@lveg5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'CollegeDespamifierAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT')
     }
 }
 
